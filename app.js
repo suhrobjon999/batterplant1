acd9675433faa2c7137a57e200b1ba8de7172898
Suhrobjon601 <suhrobjonn0343@gmail.com>
2024-07-28 20:20:06 +0500

    yangi

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const hamburger = document.querySelector('.hamburger-menu');
-    const navMenu = document.querySelector('.nav-menu');
-    const karzinka = document.querySelector('.karzinka');
-    const shop = document.querySelector('.shop')
-    const shopping = document.querySelector('.shopping')
-    
-    hamburger.addEventListener('click', () => {
-      navMenu.classList.toggle('hide');
-    });
-    karzinka.addEventListener('click', () => {
-      
-    });
+const navMenu = document.querySelector('.nav-menu');
+const karzinka = document.querySelector('.karzinka');
+const shop = document.querySelector('.shop')
+const shopping = document.querySelector('.shopping')
+const closee = document.querySelector('.closee')
+const shops = document.querySelector('.shops')
+const shops1 = document.querySelector('.shops1')
+const shops2 = document.querySelector('.shops2')
+const shops3 = document.querySelector('.shops3')
+const shops4 = document.querySelector('.shops4')
+const shops5 = document.querySelector('.shops5')
+const shops6 = document.querySelector('.shops6')
+const shops7 = document.querySelector('.shops7')
+const shops8 = document.querySelector('.shops8')
+const imgs1 = document.querySelector('.imgs1')
+const imgss2 = document.querySelector('.imgss2')
+const imgss3 = document.querySelector('.imgss3')
+const imgss4 = document.querySelector('.imgss4')
+const imgss5 = document.querySelector('.imgss5')
+const imgss6 = document.querySelector('.imgss6')
+const imgss7 = document.querySelector('.imgss7')
+const imgss8 = document.querySelector('.imgss8')
+const imgss9 = document.querySelector('.imgss9')
 
-    const rangeInput = document.querySelectorAll(".range-input input"),
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('hide');
+});
+karzinka.addEventListener('click', () => {
+
+});
+
+const rangeInput = document.querySelectorAll(".range-input input"),
   priceInput = document.querySelectorAll(".price-input input"),
   range = document.querySelector(".slider .progress");
 let priceGap = 1000;
@@ -52,6 +71,36 @@ rangeInput.forEach((input) => {
     }
   });
 });
-karzinka.addEventListener("click", ()=>{
-shopping.style.transform = 'scale(1)'
+karzinka.addEventListener("click", () => {
+  shopping.style.transform = 'scale(1)'
+});
+closee.addEventListener("click", () => {
+  shopping.style.transform = 'scale(0)'
+});
+shops.addEventListener("click", () => {
+  imgs1.style.display = 'block'
+});
+shops1.addEventListener("click", () => {
+  imgss2.style.display = 'block'
+});
+shops2.addEventListener("click", () => {
+  imgss3.style.display = 'block'
+});
+shops3.addEventListener("click", () => {
+  imgss4.style.display = 'block'
+});
+shops4.addEventListener("click", () => {
+  imgss5.style.display = 'block'
+});
+shops5.addEventListener("click", () => {
+  imgss6.style.display = 'block'
+});
+shops6.addEventListener("click", () => {
+  imgss7.style.display = 'block'
+});
+shops7.addEventListener("click", () => {
+  imgss8.style.display = 'block'
+});
+shops8.addEventListener("click", () => {
+  imgss9.style.display = 'block'
 });
